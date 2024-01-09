6bc3eda2b8276d5cc5c7802e500dec4d34ab2d7c
alirezarmj <a.rashidi.m78@gmail.com>
2024-01-09 11:14:26 +0330

    changed to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import HandDetect from "./HandDetect.jsx";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import HandDetect2 from "./HandDetect2.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route index element={<Navigate replace to="/detect2" />} />
         <Route path="/detect1" element={<HandDetect />} />
         <Route path="/detect2" element={<HandDetect2 />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
