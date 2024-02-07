66f7f6b03d4094e5dffaff9a0d684b987bc1d666
amir <amirdehghani137722@gmail.com>
2024-02-07 15:51:03 +0330

    change bakc address

--- src/api/Flow.js
+++ src/api/Flow.js
@@ -3,16 +3,17 @@ import Api from "./api";
 class Flow extends Api {
     static async chat(data) {
         let response = await fetch(
-        'https://codie-backend.azurewebsites.net/mobile/flow_uni',
+        'https://vercel-backend-one-roan.vercel.app/mobile/flow_uni',
         {
             method: 'POST',
             mode: 'cors', // no-cors, *cors, same-origin
             headers: {
             'Content-Type': 'application/json',
-            'Access-Control-Allow-Credentials': 'true',
-            'Access-Control-Allow-Origin': '*',
+            // 'Access-Control-Allow-Credentials': 'true',
+            // 'Access-Control-Allow-Origin': '*',
             Authorization:
-                'Bearer ' + localStorage.getItem('accessToken'),
+                'Bearer ' + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIwMTFiN2I5MjlhIiwiaWF0IjoxNzA3MDIzMTY5LCJuYmYiOjE3MDcwMjMxNjksImp0aSI6Ijk0ZTlkNzA4LWZiNzYtNGJkOS1hZTA0LTliNmE5MzU4MmQzZiIsImV4cCI6MTcxNzM5MTE2OSwidHlwZSI6ImFjY2VzcyIsImZyZXNoIjpmYWxzZX0.KIaBTQR8MuWXPaPrm7R2-8QWuljT4r7ovo7bVMkYOrc',
+            // 'Content-Type': 'application/x-www-form-urlencoded',
             // 'Content-Type': 'application/x-www-form-urlencoded',
             },
             body: JSON.stringify(data),
