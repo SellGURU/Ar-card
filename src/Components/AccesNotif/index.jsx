820c00582a7703e6fe602b7e21c7f4b7b87cf8dc
amir <amirdehghani137722@gmail.com>
2024-10-02 00:18:55 +0330

    fix ar notif

--- src/Components/AccesNotif/index.jsx
+++ src/Components/AccesNotif/index.jsx
@@ -21,29 +21,52 @@ const AccesNotif = () => {
     const [showNotif,setShowNotif] = useState(false)
     const [searchParams] = useSearchParams();
     
-    console.log(searchParams.get("view"))
+    // console.log(searchParams.get("view"))
     useEffect(() => {
-        if(appContext.userAccount.type == 'pro' && getDaysBetweenDates(appContext.userAccount.end_of_date) <=400 && searchParams.get("view") =='review') {
+        if(resolveText()!= undefined){
             setShowNotif(true)
         }
+        console.log(resolveText())
     },[appContext.userAccount.type])
+
+    const resolveText = () => {
+        if((appContext.userAccount.type == 'trial' || appContext.userAccount.type == 'free') && searchParams.get("view") =='review'){
+            return "Upgrade to Pro"
+        }        
+        if(appContext.userAccount.type == 'pro'){
+            if(getDaysBetweenDates(appContext.userAccount.end_of_date) <=7 && searchParams.get("view") =='review'){
+                return 'AR features will be limited soon. Renew to  allow your followers experience cutting-edge technology.'
+            }
+        }
+        if(searchParams.get("view") !='review'){
+            return "Impressed by what you heard? Imagine your voice here. Start your free Avatalk trial today!"
+
+        }
+
+    }
     return (
         <>
         {showNotif &&
             <div className="w-full absolute bottom-16 flex justify-center">
-                <div className="w-full max-w-[366px] rounded-[27px] p-4 px-7 text-justify bg-white border border-[#6432C9]">
-                    <div className="text-[#374151] text-[14px]">
-                        AR features will be limited soon. Renew to  allow your followers experience cutting-edge technology.
+                {resolveText() =='Upgrade to Pro' ?
+                    <div onClick={() => {
+                        window.open("https://portal.avatalk.me/#/settings/service")
+                    }} className="bg-primary-color w-[140px] h-[40px] text-white flex justify-center items-center rounded-[27px] cursor-pointer">{'Upgrade to Pro'}</div>
+                :
+                    <div className="w-full max-w-[366px] rounded-[27px] p-4 px-7 text-justify bg-white border border-[#6432C9]">
+                        <div className="text-[#374151] text-[14px]">
+                            {resolveText()}
+                        </div>
+                        <div className="w-full flex gap-6 justify-end mt-2">
+                            <div onClick={() => {
+                                window.open("https://portal.avatalk.me/#/settings/service")
+                            }} className="text-[#FBBF24] cursor-pointer text-[14px] font-medium">{appContext.userAccount.type == 'pro'?'Renew Your Plan':'Create Your Avatalk'}</div>
+                            <div onClick={() => {
+                                setShowNotif(false)
+                            }} className="text-[#94A3B8] cursor-pointer text-[14px] font-medium">Skip Now</div>
+                        </div>
                     </div>
-                    <div className="w-full flex gap-6 justify-end mt-2">
-                        <div onClick={() => {
-                            window.open("https://portal.avatalk.me/#/settings/service")
-                        }} className="text-[#FBBF24] cursor-pointer text-[14px] font-medium">Renew Your Plan</div>
-                        <div onClick={() => {
-                            setShowNotif(false)
-                        }} className="text-[#94A3B8] cursor-pointer text-[14px] font-medium">Skip Now</div>
-                    </div>
-                </div>
+                }
             </div>
         }
         </>
